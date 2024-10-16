8c0031ad4b13843dc790913a44731427cc2c687b
Asiya <asiyacse504@gmail.com>
2024-10-16 03:52:24 -0700

    Create ChatMessage.js

---
+++ ChatMessage.js
@@ -0,0 +1,12 @@
+
+const mongoose = require('mongoose');
+
+const chatMessageSchema = new mongoose.Schema({
+    user: { type: String, required: true },
+    message: { type: String, required: true },
+
+});
+
+const ChatMessage = mongoose.model('ChatMessage', chatMessageSchema);
+module.exports = ChatMessage;
+
